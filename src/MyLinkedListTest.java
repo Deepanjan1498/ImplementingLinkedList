b42251d142cacf2e58cba439c24c4c2f71d42c14
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-14 06:13:51 +0530

    [Deepanjan]Add . uc4 is solved and ability to insert a number in linked list is used.

--- src/MyLinkedListTest.java
+++ src/MyLinkedListTest.java
@@ -36,5 +36,20 @@ public class MyLinkedListTest
     					myLinkedList.tail.equals(myThirdNode);
     	Assert.assertTrue(result);
 	}
-
+	@Test
+	public void givenNumberWhenInsertedInLinkedListShouldReturnTheList() 
+	{
+		MyNode<Integer> myFirstNode=new MyNode<Integer>(56);
+    	MyNode<Integer> mySecondNode=new MyNode<Integer>(30);
+    	MyNode<Integer> myThirdNode=new MyNode<Integer>(70);
+    	MyLinkedList myLinkedList=new MyLinkedList();
+    	myLinkedList.add(myFirstNode);
+    	myLinkedList.append(myThirdNode);
+    	myLinkedList.insert(myFirstNode,mySecondNode);
+    	myLinkedList.printMyNode();
+    	boolean result=myLinkedList.head.equals(myFirstNode)&&
+    					myLinkedList.head.getNext().equals(mySecondNode)&&
+    					myLinkedList.tail.equals(myThirdNode);
+    	Assert.assertTrue(result);
+	}
 }
