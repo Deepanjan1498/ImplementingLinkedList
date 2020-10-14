84a0d93ea1cbe7db5569159e01dc0df101975b5c
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-14 06:32:05 +0530

    [Deepanjan]Add . uc6 is solved and last element deletion feature is used.

--- src/MyNodeTest.java
+++ src/MyNodeTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class MyNodeTest 
 {
     @Test
-    public void given3NumbersWhenLinked_ShouldPass_LinkedListTest()
+    public void givenThreeNumbersWhenLinkedShouldReturnLinkedListTest()
     {
     	MyNode<Integer> myFirstNode=new MyNode<Integer>(56);
     	MyNode<Integer> mySecondNode=new MyNode<Integer>(30);
